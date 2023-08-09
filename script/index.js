760a58588b19f9f1d3a08bf042e0ad7e8d7721d4
anasgee <anasraza6096@gmail.com>
2023-08-10 01:08:43 +0500

    added all files to stagged area at once

---
+++ script/index.js
@@ -0,0 +1,69 @@
+// Menu Hamburger
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.addEventListener("click", () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+});
+
+//
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+// To change the active class on navLinks a
+
+// to change navlink active class to different sections
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 100;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href *= " + id + " ")
+          .classList.add("active");
+      });
+    }
+  });
+
+  // Sticky Navbar
+
+  let header = document.querySelector("header");
+
+  header.classList.toggle("sticky", window.scrollY > 100);
+  // ----------------------To Remove Toggle Nav When click on link or cross ------------------------------//
+
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
+};
+
+// Scroll Reveal ////////
+ScrollReveal({ 
+  reset: true,
+  distance:'80px',
+  duration:2000,
+  delay:200
+});
+ScrollReveal().reveal('.home-content, .heading' , {origin:'top'});
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form' , {origin:'bottom'});
+ScrollReveal().reveal('.home-content h1, .about-img' , {origin:'left'});
+ScrollReveal().reveal('.home-content p, .about-content' , {origin:'right'});
+
+// Typ ed Js
+document.querySelectorAll('.multiple-text').forEach(function(elem){
+  new Typed(elem,{
+    strings: ['Frontend Developer','React Developer', 'Youtuber', 'Graphic Designer'],
+    typeSpeed: 50,
+    backSpeed:50,
+    backDelay:1000,
+    loop:true
+  })
+
+})
